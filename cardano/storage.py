78d601b045fb51a62f316300c662611b5208f348
yihuang <yi.codeplayer@gmail.com>
2018-07-22 18:37:40 +0800

    Store forward link of blocks.

--- cardano/storage.py
+++ cardano/storage.py
@@ -5,8 +5,12 @@
     genesis -> hash of genesis block of epoch.
     tip -> hash of last block of epoch.
 * Main database:
-  * 'b' + hash -> BlockHeader
-  * 'c' + tip -> hash
+  * 'c/tip' -> hash
+  * 'b/' + hash -> BlockHeader
+  * 'e/fl/' + hash -> hash of next block.
+  * 'ut/t/' + txIn -> TxOut
+  * 's/' + stake holder id
+  * 's/ftssum'
 
 Sync
 ----
@@ -17,6 +21,7 @@ Sync
 '''
 import os
 import binascii
+import itertools
 
 import cbor
 import rocksdb
@@ -27,6 +32,7 @@ class Storage(object):
     def __init__(self, root_path, readonly=False):
         self._root_path = root_path
         self.db = rocksdb.DB(os.path.join(self._root_path, 'db'), rocksdb.Options(create_if_missing=True), readonly)
+        self._tip = None # cache current tip in memory.
 
     def epoch_db_path(self, epoch):
         return os.path.join(self._root_path, 'epoch%d'%epoch)
@@ -35,10 +41,16 @@ class Storage(object):
         return rocksdb.DB(self.epoch_db_path(epoch), rocksdb.Options(create_if_missing=True), readonly)
 
     def tip(self):
-        return self.db.get(b'c/tip')
+        if not self._tip:
+            self._tip = self.db.get(b'c/tip')
+        return self._tip
 
     def set_tip(self, s):
-        return self.db.put(b'c/tip', s)
+        self._tip = s
+        self.db.put(b'c/tip', s)
+
+    def genesis_block_hash(self):
+        return self.open_epoch_db(0).get(b'genesis')
 
     def blockheader(self, hash):
         buf = self.db.get(b'b/'+hash)
@@ -51,46 +63,86 @@ class Storage(object):
         if buf:
             return DecodedBlock(cbor.loads(buf), buf)
 
-    def blocks_rev(self):
-        current_epoch = None
-        current_epoch_db = None
-
-        h = self.tip()
+    def blocks_rev(self, start_hash=None):
+        'Iterate blocks backwardly.'
+        current_hash = start_hash or self.tip()
+        current_epoch = self.blockheader(current_hash).slot()[0]
+        current_epoch_db = self.open_epoch_db(current_epoch, readonly=True)
         while True:
-            hdr = self.blockheader(h)
-            epoch = hdr.slot()[0]
-            if epoch != current_epoch:
-                print('epoch', epoch)
-                current_epoch = epoch
-                current_epoch_db = self.open_epoch_db(epoch, readonly=True)
-            buf = current_epoch_db.get(h)
-            assert buf, 'missing block: ' + binascii.hexlify(h)
-            yield DecodedBlock(cbor.loads(buf), buf)
-
-            if hdr.slot() == (0, None):
-                break
-            h = hdr.prev_header()
-
-    def blocks(self):
-        blocks = []
-        max_epoch = self.blockheader(self.tip()).slot()[0]
-        for epoch in range(max_epoch):
-            epoch_db = self.open_epoch_db(epoch, readonly=True)
-            h = epoch_db.get(b'tip')
-            if not h:
-                break
-            blocks = []
-            while True:
-                buf = epoch_db.get(h)
-                blk = DecodedBlock(cbor.loads(buf), buf)
-                blocks.append(blk)
-                hdr = blk.header()
-                h = hdr.prev_header()
-                if hdr.is_genesis():
+            raw = current_epoch_db.get(current_hash)
+            if not raw:
+                # try decrease epoch id.
+                current_epoch -= 1
+                if current_epoch < 0:
                     break
+                current_epoch_db = self.open_epoch_db(current_epoch, readonly=True)
+                continue
 
-            for blk in reversed(blocks):
-                yield blk
+            blk = DecodedBlock(cbor.loads(raw), raw)
+            yield blk
+            current_hash = blk.header().prev_header()
+
+    def blocks(self, start_hash=None):
+        'Iterate blocks forwardly.'
+        if start_hash:
+            current_epoch, _ = DecodedBlockHeader(cbor.loads(self.db.get(b'b/'+start_hash))).slot()
+        else:
+            start_hash = self.genesis_block_hash()
+            current_epoch = 0
+
+        current_epoch_db = self.open_epoch_db(current_epoch, readonly=True)
+        current_hash = start_hash
+        raw = current_epoch_db.get(current_hash)
+        yield DecodedBlock(cbor.loads(raw), raw)
+        while True:
+            current_hash = self.db.get(b'e/fl/' + current_hash)
+            if not current_hash:
+                return
+
+            raw = current_epoch_db.get(current_hash)
+            if raw:
+                yield DecodedBlock(cbor.loads(raw), raw)
+                continue
+
+            # try increase epoch number.
+            current_epoch += 1
+            current_epoch_db = self.open_epoch_db(current_epoch, readonly=True)
+            if not current_epoch_db:
+                return
+            raw = current_epoch_db.get(current_hash)
+            if not raw:
+                return
+
+            yield DecodedBlock(cbor.loads(raw), raw)
+
+    def blockheaders_rev(self):
+        'Iterate block header backwardly.'
+        current_hash = self.tip()
+        while True:
+            raw = self.db.get(b'b/'+current_hash)
+            if not raw:
+                break
+            hdr = DecodedBlockHeader(cbor.loads(raw), raw)
+            yield hdr
+            current_hash = hdr.prev_header()
+
+    def blockheaders(self):
+        current_hash = self.genesis_block_hash()
+        while True:
+            raw = self.db.get(b'b/'+current_hash)
+            yield DecodedBlockHeader(cbor.loads(raw), raw)
+            current_hash = self.db.get(b'e/fl/'+current_hash)
+            if not current_hash:
+                break
+
+    def blockheaders_noorder(self):
+        'Iterate block header in rocksdb order, fastest.'
+        it = self.db.iteritems()
+        it.seek(b'b/')
+        for k, raw in it:
+            if not k.startswith(b'b/'):
+                break
+            yield DecodedBlockHeader(cbor.loads(raw), raw)
 
     def append_block(self, block):
         hdr = block.header()
@@ -102,4 +154,5 @@ class Storage(object):
 
         hash = hdr.hash()
         self.db.put(b'b/' + hash, hdr.raw())
-        self.db.put(b'c/tip', hash)
+        self.db.put(b'e/fl/' + hdr.prev_header(), hash)
+        self.set_tip(hash)
