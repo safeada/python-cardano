dc1c4203602b562ae5c42bf2f0e9dfa87dc12450
huangyi <huang@crypto.com>
2021-05-24 12:26:27 +0800

    basic shelley era transport protocol

--- cardano/utils.py
+++ cardano/utils.py
@@ -1,10 +1,7 @@
-import hashlib
-import time
-import cbor
 import getpass
+import hashlib
 
-from . import config
-from . import cbits
+import cbor2
 
 
 def hash_serialized(s):
@@ -12,59 +9,39 @@ def hash_serialized(s):
 
 
 def hash_data(v):
-    return hash_serialized(cbor.dumps(v))
-
-
-def flatten_slotid(slot):
-    epoch, idx = slot
-    return epoch * config.SECURITY_PARAMETER_K * 10 + (idx or 0)
-
-
-def unflatten_slotid(n):
-    return divmod(n, config.SECURITY_PARAMETER_K * 10)
-
-
-def get_current_slot():
-    n = (int(time.time()) - config.START_TIME) // (config.SLOT_DURATION / 1000)
-    return unflatten_slotid(n)
+    return hash_serialized(cbor2.dumps(v))
 
 
 def input_passphase():
-    passphase = getpass.getpass('Input passphase:').encode()
+    passphase = getpass.getpass("Input passphase:").encode()
     if passphase:
         return hashlib.blake2b(passphase, digest_size=32).digest()
     return passphase
 
 
 SIGN_TAGS = {
-    'test': b'\x00',
-    'tx': b'\x01',
-    'redeem_tx': b'\x02',
-    'vss_cert': b'\x03',
-    'us_proposal': b'\x04',
-    'commitment': b'\x05',
-    'us_vote': b'\x06',
-    'mainblock': b'\x07',
-    'mainblock_light': b'\x08',
-    'mainblock_heavy': b'\x09',
-    'proxy_sk': b'\x0a',
+    "test": b"\x00",
+    "tx": b"\x01",
+    "redeem_tx": b"\x02",
+    "vss_cert": b"\x03",
+    "us_proposal": b"\x04",
+    "commitment": b"\x05",
+    "us_vote": b"\x06",
+    "mainblock": b"\x07",
+    "mainblock_light": b"\x08",
+    "mainblock_heavy": b"\x09",
+    "proxy_sk": b"\x0a",
 }
 
 
-def sign_tag(tag):
-    if tag is None:
-        return b''
-    elif tag == 'test':
-        return b'\x00'
-    else:
-        return SIGN_TAGS[tag] + cbor.dumps(config.PROTOCOL_MAGIC)
+def recv_exact(sock, n):
+    buf = b""
+    while len(buf) < n:
+        s = sock.recv(n - len(buf))
+        assert s, "connection closed"
+        buf += s
+    return buf
 
 
-def sign(tag, sk, data):
-    msg = sign_tag(tag) + cbor.dumps(data)
-    return cbits.encrypted_sign(sk, b'', msg)
-
-
-def verify(tag, pk, sig, data):
-    msg = sign_tag(tag) + cbor.dumps(data)
-    return cbits.verify(pk, msg, sig)
+def send_many(o, *args):
+    o.sendall(b"".join(args))
